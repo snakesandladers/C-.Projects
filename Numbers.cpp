860578df8e17b97bd7715eaaae1be2d9c3e8a7be
snakesandladers <93540215+snakesandladers@users.noreply.github.com>
2022-02-10 21:37:41 +0530

    Create Numbers.cpp

---
+++ Numbers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    double num1,num2,product;
+    cout<<"enter two numbers :";
+    cin>>num1 >> num2;
+    product=num1*num2;
+    cout<<"product="<<product;
+
+    return 0;
+}
+output:
+enter two numbers :3.4
+5.5
+product=18.7
+  
