4d413fc5e4c335b80dfa418166f5cacc54e3c48f
snakesandladers <93540215+snakesandladers@users.noreply.github.com>
2022-02-10 21:35:23 +0530

    Create size.cpp

---
+++ size.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std ;
+
+
+int main()
+{
+cout<<"size of char: "<<sizeof(char)<< "byte"<<endl;
+cout<<"size of int: "<<sizeof(int)<< "bytes"<<endl;
+cout<<"size of float: "<<sizeof(float)<< "bytes"<<endl;
+cout<<"size of double: "<<sizeof(double)<< "bytes"<<endl;
+
+return 0;
+
+}
+
+output:
+size of char: 1byte
+size of int: 4bytes
+size of float: 4bytes
+size of double: 8bytes
